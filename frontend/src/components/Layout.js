cf907bd37691b0e5f086ca77140b7ee4e230d7e6
saurabh-shelke1993 <shelke.saurabh017@gmail.com>
2026-09-26 19:25:16 +0530

    feat: establish visual application shell and dashboard foundation

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,32 +1,13 @@
 import Sidebar from "./Sidebar";
 
 function Layout({ children }) {
-
   return (
-
-    <div
-      style={{
-        display: "flex",
-        minHeight: "100vh",
-        backgroundColor: "#e5e7eb",
-      }}
-    >
-
-      {/* SIDEBAR */}
-
+    <div className="app-shell">
       <Sidebar />
 
-      {/* PAGE CONTENT */}
-
-      <div
-        style={{
-          flex: 1,
-          padding: "30px",
-        }}
-      >
-        {children}
-      </div>
-
+      <main className="app-main">
+        <div className="app-content">{children}</div>
+      </main>
     </div>
   );
 }
